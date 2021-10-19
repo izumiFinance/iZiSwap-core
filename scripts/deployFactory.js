9d878f84f1dc906ba10001495a84946f427578b8
opcc <opcc22059@gmail.com>
2021-10-19 15:43:46 +0800

    deploy script

--- scripts/deployFactory.js
+++ scripts/deployFactory.js
@@ -1,9 +1,21 @@
 const { ethers } = require("hardhat");
 
+async function getPoolParts() {
+  const IzumiswapPoolPartFactory = await ethers.getContractFactory("IzumiswapPoolPart");
+  const izumiswapPoolPart = await IzumiswapPoolPartFactory.deploy();
+  await izumiswapPoolPart.deployed();
+  const IzumiswapPoolPartDesireFactory = await ethers.getContractFactory("IzumiswapPoolPartDesire");
+  const izumiswapPoolPartDesire = await IzumiswapPoolPartDesireFactory.deploy();
+  await izumiswapPoolPartDesire.deployed();
+  return [izumiswapPoolPart.address, izumiswapPoolPartDesire.address];
+}
 async function main() {
     const IzumiswapFactory = await ethers.getContractFactory("IzumiswapFactory");
+    [poolPart, poolPartDesire] = await getPoolParts();
 
-    const factory = await IzumiswapFactory.deploy();
+    console.log("pool part: ", poolPart);
+    console.log("pool part desire: ", poolPartDesire);
+    const factory = await IzumiswapFactory.deploy(poolPart, poolPartDesire);
     await factory.deployed();
 
     console.log("factory addr: " + factory.address);
