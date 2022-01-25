c35bbd9e1b106275363d44ff745bd6b3484331dc
opcc <opcc22059@gmail.com>
2022-01-25 20:15:59 +0800

    update

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -1,6 +1,6 @@
 module.exports = {
-    izumi_test: {
-        factory: '0x2386Ce9B4d600EA731a1C7c5A819F816b863deca',
+    izumiTest: {
+        factory: '0x80E1Fcd031355F55d8Df9625927B107817B268bB',
         BIT: '0x41BC21bdcF0FA87ae6eeFcBE0e4dB29dB2b650C1',
         USDC: '0xe507AAC9eFb2A08F53C7BC73B3B1b8BCf883E41B',
     },
