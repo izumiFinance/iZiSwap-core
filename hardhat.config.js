525856cebf998669e6d2aec105235ff934afe405
opcc <opcc22059@gmail.com>
2022-03-31 17:10:52 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,20 +10,11 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.7.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
-        }
-      },
-      {
         version: "0.8.4",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
+            runs: 20
           }
         }
       },
