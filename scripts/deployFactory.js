21aa001a712a6102c44f77c80b1db8995d4518a4
opcc <opcc22059@gmail.com>
2022-04-19 16:19:33 +0800

    update

--- scripts/deployFactory.js
+++ scripts/deployFactory.js
@@ -1,27 +1,44 @@
 const { ethers } = require("hardhat");
 
 async function getPoolParts() {
-  const iZiSwapPoolPartFactory = await ethers.getContractFactory("SwapX2YModule");
-  const iZiSwapPoolPart = await iZiSwapPoolPartFactory.deploy();
-  await iZiSwapPoolPart.deployed();
-  const iZiSwapPoolPartDesireFactory = await ethers.getContractFactory("SwapY2XModule");
-  const iZiSwapPoolPartDesire = await iZiSwapPoolPartDesireFactory.deploy();
-  await iZiSwapPoolPartDesire.deployed();
+  const SwapX2YModuleFactory = await ethers.getContractFactory("SwapX2YModule");
+  const swapX2YModule = await SwapX2YModuleFactory.deploy();
+  await swapX2YModule.deployed();
+  
+  const SwapY2XModuleFactory = await ethers.getContractFactory("SwapY2XModule");
+  const swapY2XModule = await SwapY2XModuleFactory.deploy();
+  await swapY2XModule.deployed();
+
   const MintModuleFactory = await ethers.getContractFactory('MintModule');
   const mintModule = await MintModuleFactory.deploy();
   await mintModule.deployed();
-  return [iZiSwapPoolPart.address, iZiSwapPoolPartDesire.address, mintModule.address];
+
+  const LimitOrderModuleFactory = await ethers.getContractFactory('LimitOrderModule');
+  const limitOrderModule = await LimitOrderModuleFactory.deploy();
+  await limitOrderModule.deployed();
+  return {
+    swapX2YModule: swapX2YModule.address,
+    swapY2XModule: swapY2XModule.address,
+    mintModule: mintModule.address,
+    limitOrderModule: limitOrderModule.address,
+  };
 }
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log(deployer.address);
-    const iZiSwapFactory = await ethers.getContractFactory("iZiSwapFactory");
-    const [swapX2Y, swapY2X, mintModule] = await getPoolParts();
+    const {swapX2YModule, swapY2XModule, mintModule, limitOrderModule} = await getPoolParts();
 
-    console.log("x2y: ", swapX2Y);
-    console.log("y2x: ", swapY2X);
-    console.log('mint: ', mintModule);
-    const factory = await iZiSwapFactory.deploy(deployer.address, swapX2Y, swapY2X, mintModule);
+    // deploy a factory
+    const iZiSwapFactory = await ethers.getContractFactory("iZiSwapFactory");
+
+    const receiverAddress = '0xa064411B9F927226FB4a99864a247b1ef991b04F';
+
+    console.log('swapX2YModule: ', swapX2YModule)
+    console.log('swapY2XModule: ', swapY2XModule)
+    console.log('mintModule: ', mintModule)
+    console.log('limitOrderModule: ', limitOrderModule)
+
+    const factory = await iZiSwapFactory.deploy(receiverAddress, swapX2YModule, swapY2XModule, mintModule, limitOrderModule);
     await factory.deployed();
 
     console.log("factory addr: " + factory.address);
