cae3bbe8dcd200c2ed49e6aad4d016bedc7ad8eb
McGrady00H <281130306@qq.com>
2022-04-27 22:15:21 +0800

    update

--- contracts/test/TestMulDivMath.sol
+++ contracts/test/TestMulDivMath.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.4;
 import '../libraries/MulDivMath.sol';
 
 contract TestMulDivMath {
-    function getMulDivFloor(uint256 a, uint256 b, uint256 c) external pure returns (uint256) {
-        return mulDivMath.mulDivFloor(a, b, c);
+    function getMulDivFloor(uint256 a, uint256 b, uint256 c) external view returns (uint256) {
+        return MulDivMath.mulDivFloor(a, b, c);
     }
 
-    function getGasCostOfMulDivFloor(uint256 a, uint256 b, uint256 c) external pure returns (uint256) {
+    function getGasCostOfMulDivFloor(uint256 a, uint256 b, uint256 c) external view returns (uint256) {
         uint256 gasBefore = gasleft();
-        mulDivMath.mulDivFloor(a, b, c);
+        MulDivMath.mulDivFloor(a, b, c);
         return gasBefore - gasleft();
     }
 
-    function getMulDivCeil(uint256 a, uint256 b, uint256 c) external pure returns (uint256) {
-        return mulDivMath.mulDivCeil(a, b, c);
+    function getMulDivCeil(uint256 a, uint256 b, uint256 c) external view returns (uint256) {
+        return MulDivMath.mulDivCeil(a, b, c);
     }
 
-    function getGasCostOfMulDivCeil(uint256 a, uint256 b, uint256 c) external pure returns (uint256) {
+    function getGasCostOfMulDivCeil(uint256 a, uint256 b, uint256 c) external view returns (uint256) {
         uint256 gasBefore = gasleft();
-        mulDivMath.mulDivCeil(a, b, c);
+        MulDivMath.mulDivCeil(a, b, c);
         return gasBefore - gasleft();
     }
 }
--- test/mulDivMath/testMulDivMath.js
+++ test/mulDivMath/testMulDivMath.js
@@ -1,109 +1,169 @@
-const { expect, use } = require("chai");
-const { ethers } = require("hardhat");
+const {
+    expect,
+    use
+} = require("chai");
+const {
+    ethers
+} = require("hardhat");
 const BigNumber = require('bignumber.js');
 
 
-describe("log pow math", function () {
+describe("Mul Div Math", function () {
     var signer, miner1, miner2, trader, seller, receiver;
     var contract;
     var q128;
     var q256;
-    beforeEach(async function() {
+    beforeEach(async function () {
         [signer, miner1, miner2, miner3, trader, seller, receiver] = await ethers.getSigners();
 
-        const logPowMathContractFactory = await ethers.getContractFactory("TestLogPowMath");
-        contract = await logPowMathContractFactory.deploy();
+        const mulDivMathContractFactory = await ethers.getContractFactory("TestMulDivMath");
+        contract = await mulDivMathContractFactory.deploy();
 
-        q256 = BigNumber(2).pow(256).toFixed(0);
-        q128 = BigNumber(2).pow(128).toFixed(0);
+        q256 = BigNumber(2).pow(256);
+        q128 = BigNumber(2).pow(128);
 
     });
 
-    it ("test gas cost", async function () {
-        const res = (await contract.getGasCostOfGetSqrtPrice(-70010)).toString();
+    it("test gas cost", async function () {
+        let a = new BigNumber(1231232323211).times(Math.floor(Math.random() * 10000)).times(q128).toFixed(0);
+        let b = new BigNumber(12312323232112123132311).times(q128).toFixed(0);
+        let c = new BigNumber(23232112123132311).times(q128).toFixed(0);
+        const res = (await contract.getGasCostOfMulDivFloor(a, b, c)).toString();
         console.log('   gas cost: ', res);
     });
 
-    // it("test getLogSqrtPrice precision ....", async function () {
-    //     const res = (await contract.getSqrtPrice(700000)).toString();
-    //     //const a = (new BigNumber(1.0001).pow(-8000)).pow(10).pow(10).sqrt().times(2**96);
-    //     const a = (new BigNumber(1.0001 ** (700000))).sqrt().times(2**96).toFixed(0);
+    it("test mul div floor", async function () {
+        let a = new BigNumber(1231232323211).times(Math.floor(Math.random() * 10000)).times(q128);
+        let b = new BigNumber(12312323232112123132311).times(q128);
+        let c = new BigNumber(23232112123132311).times(q128);
+        let d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 3));
+        if (d.lt(2 ** 256)) {
+            let res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            let e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
 
-    //     console.log(a);
-    //     console.log(res);
-    //     console.log(2**(-96));
+        a = new BigNumber(12312323231).times(Math.floor(Math.random() * 10000)).times(q128);
+        b = new BigNumber(123123232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        c = new BigNumber(23232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+        d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 3));
+        if (d.lt(2 ** 256)) {
+            res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
 
-    // });
+        a = new BigNumber(12312323231).times(2 ** 49).times(Math.floor(Math.random() * 10000)).times(q128);
+        b = new BigNumber(123123232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        c = new BigNumber(23232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 3));
+        if (d.lt(2 ** 256)) {
+            res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
 
-    // it("test first getSqrtPrice and then log ....", async function () {
-    //     for (var i=700001; i< 800000; i ++) {
-    //         if (i % 1000 === 0){
-    //             console.log(i);
-    //         }
-    //         //   const a = await contract.getSqrtPrice(i);
-    //         //   const res = await contract.getLogSqrtPriceFU(a);
-    //         //   expect(res[0]).to.equal(i-1);
-    //         //   expect(res[0] + 1).to.equal(res[1]);
+        a = new BigNumber(912312323231).times(2 ** 49).times(Math.floor(Math.random() * 10000)).times(q128);
+        b = new BigNumber(8123123232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        c = new BigNumber(7232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 3));
+        if (d.lt(2 ** 256)) {
+            res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
 
-    //           let a2 = await contract.getSqrtPrice(-i);
-    //           a2 = new BigNumber(a2.toString()).div(1.0001 ** 0.499999);
-    //           const res2 = await contract.getLogSqrtPriceFU(a2.toFixed(0));
-    //           const res3 = await contract.getSqrtPrice(res2[0]);
-    //           if (new BigNumber(res3.toString()) > a2) {
-    //               console.log(a2.toFixed(0));
-    //               console.log(res3.toString());
-    //               console.log(i);
-    //           }
-    //     }
+        a = new BigNumber(12312323231).times(2 ** 49).times(Math.floor(Math.random() * 10000)).times(q128);
+        b = new BigNumber(123123232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        c = new BigNumber(23232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 3));
+        if (d.lt(2 ** 256)) {
+            res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
+    });
 
-    //     // const res = await contract.getSqrtPrice(400000);
-    //     // //const a = (new BigNumber(1.0001).pow(-8000)).pow(10).pow(10).sqrt().times(2**96);
-    //     // const a = (new BigNumber(1.0001 ** (400000))).sqrt().times(2**96);
-    //     // console.log(a);
-    //     // console.log(res);
-    // });
+    it("test mul div ceil", async function () {
+        let a = new BigNumber(1231232323211).times(Math.floor(Math.random() * 10000)).times(q128);
+        let b = new BigNumber(12312323232112123132311).times(q128);
+        let c = new BigNumber(23232112123132311).times(q128);
+        let d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 2));
+        if (d.lt(2 ** 256)) {
+            let res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            let e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
 
-    // it("test first getSqrtPrice and then log ....", async function () {
-    //     for (var i=700001; i< 800000; i ++) {
-    //         if (i % 1000 === 0){
-    //             console.log(i);
-    //         }
-    //         //   const a = await contract.getSqrtPrice(i);
-    //         //   const res = await contract.getLogSqrtPriceFU(a);
-    //         //   expect(res[0]).to.equal(i-1);
-    //         //   expect(res[0] + 1).to.equal(res[1]);
+        a = new BigNumber(12312323231).times(Math.floor(Math.random() * 10000)).times(q128);
+        b = new BigNumber(123123232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        c = new BigNumber(23232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 2));
+        if (d.lt(2 ** 256)) {
+            res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
 
-    //           let a2 = await contract.getSqrtPrice(-i);
-    //           a2 = new BigNumber(a2.toString()).div(1.0001 ** 0.499999);
-    //           const res2 = await contract.getLogSqrtPriceFU(a2.toFixed(0));
-    //           const res3 = await contract.getSqrtPrice(res2[0]);
-    //           if (new BigNumber(res3.toString()) > a2) {
-    //               console.log(a2.toFixed(0));
-    //               console.log(res3.toString());
-    //               console.log(i);
-    //           }
-    //     }
+        a = new BigNumber(12312323231).times(2 ** 49).times(Math.floor(Math.random() * 10000)).times(q128);
+        b = new BigNumber(123123232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        c = new BigNumber(23232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 2));
+        if (d.lt(2 ** 256)) {
+            res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
 
-    //     // const res = await contract.getSqrtPrice(400000);
-    //     // //const a = (new BigNumber(1.0001).pow(-8000)).pow(10).pow(10).sqrt().times(2**96);
-    //     // const a = (new BigNumber(1.0001 ** (400000))).sqrt().times(2**96);
-    //     // console.log(a);
-    //     // console.log(res);
-    // });
+        a = new BigNumber(912312323231).times(2 ** 49).times(Math.floor(Math.random() * 10000)).times(q128);
+        b = new BigNumber(8123123232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        c = new BigNumber(7232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 2));
+        if (d.lt(2 ** 256)) {
+            res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
 
-    // it("test getLogSqrtPrice directly from 1.0001^i ....", async function () {
-    //     // fail at -650727
-    //     for (var i=0; i< 800000; i ++) {
-    //         if (i % 1000 === 0){
-    //             console.log(i);
-    //         }
-    //           const res = await contract.getLogSqrtPriceFU((new BigNumber(1.0001 ** i).sqrt().times(2**96)).toFixed(0));
-    //           expect(res[0]).to.equal(i-1);
-    //           //expect(res[0] + 1).to.equal(res[1]);
-
-    //           const res2 = await contract.getLogSqrtPriceFU((new BigNumber(1.0001 ** (-i)).sqrt().times(2**96)).toFixed(0));
-    //           expect(res2[0]).to.equal((-i)-1);
-    //           //expect(res2[0] + 1).to.equal(res2[1]);
-    //     }
-    // });
+        a = new BigNumber(12312323231).times(2 ** 49).times(Math.floor(Math.random() * 10000)).times(q128);
+        b = new BigNumber(123123232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        c = new BigNumber(23232112123132311).times(Math.floor(Math.random() * 10000)).times(q128);
+        d = a.times(b).div(c);
+        console.log(d.toFixed(8));
+        console.log(d.toFixed(0, 2));
+        if (d.lt(2 ** 256)) {
+            res = (await contract.getMulDivFloor(a.toFixed(0), b.toFixed(0), c.toFixed(0))).toString();
+            e = new BigNumber(res).toFixed(0);
+            console.log(e);
+            expect(e).to.equal(d.toFixed(0, 3))
+        }
+    });
 });
