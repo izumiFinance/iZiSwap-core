f7cf3291c2543c2b60c1051586d4af4130b8ce1a
opcc <opcc22059@gmail.com>
2022-05-04 21:40:13 +0800

    update

--- contracts/test/TestCalc.sol
+++ contracts/test/TestCalc.sol
@@ -4,6 +4,9 @@ import 'hardhat/console.sol';
 
 import '../libraries/Liquidity.sol';
 
+import '../libraries/LogPowMath.sol';
+import '../libraries/AmountMath.sol';
+
 contract TestCalc {
     
     function addu256(uint256 a, uint256 b) public pure returns (uint256 c) {
@@ -59,6 +62,7 @@ contract TestCalc {
     mapping(uint256=>uint256) public values;
 
     address public testCalc;
+    uint160 public sqrtRate_96;
 
     function loop(uint256 a, uint256 b) public {
         uint256 i = 0;
@@ -77,5 +81,34 @@ contract TestCalc {
     function wordBitIdx(int24 mapPt) public pure returns(int16 wordIdx, uint8 bitIdx) {
         wordIdx = int16(mapPt >> 8);
         bitIdx = uint8(uint24(mapPt % 256));
+    }
+
+    constructor() {
+        sqrtRate_96 = LogPowMath.getSqrtPrice(1);
+    }
+
+    function getAmountX(uint128 liquidity, int24 leftPt, int24 rightPt, bool upper) public view returns (uint256) {
+        uint160 sqrtPriceR_96 = LogPowMath.getSqrtPrice(rightPt);
+        return AmountMath.getAmountX(
+            liquidity,
+            leftPt,
+            rightPt,
+            sqrtPriceR_96,
+            sqrtRate_96,
+            upper
+        );
+    }
+
+    function getAmountY(uint128 liquidity, int24 leftPt, int24 rightPt, bool upper) public view returns (uint256) {
+
+        uint160 sqrtPriceL_96 = LogPowMath.getSqrtPrice(leftPt);
+        uint160 sqrtPriceR_96 = LogPowMath.getSqrtPrice(rightPt);
+        return AmountMath.getAmountY(
+            liquidity,
+            sqrtPriceL_96,
+            sqrtPriceR_96,
+            sqrtRate_96,
+            upper
+        );
     }
 }
