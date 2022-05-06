e4ca947d0ceab060953d3f0db19f7d7b5a25ac50
opcc <opcc22059@gmail.com>
2022-05-06 21:53:15 +0800

    update

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -6,5 +6,13 @@ module.exports = {
     },
     bscTest: {
         factory: '0x77132b63429718Db2B6ad8D942eE13A198f6Ab49',
+    },
+    bsc: {
+        swapX2YModule: '0xe96526e92ee57bBD468DA1721987aa988b008768',
+        swapY2XModule: '0xbD6abA1Ef82A4cD6e15CB05e95f433ef48dfb5df',
+        liquidityModule: '0xF4efDB5A1E852f78e807fAE7100B1d38351e38c7',
+        limitOrderModule: '0x32D02Fc7722E81F6Ac60B87ea8B4b63a52Ad2b55',
+        flashModule: '0x110dE362cc436D7f54210f96b8C7652C2617887D',
+        factory: '0x1502d025BfA624469892289D45C0352997251728'
     }
 }
