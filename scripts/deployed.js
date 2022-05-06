80c9277aafc20153d6406d8d3e68d098903abc0d
Mac <MacRen000@gmail.com>
2022-05-06 22:32:17 +0800

    update

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -8,11 +8,11 @@ module.exports = {
         factory: '0x77132b63429718Db2B6ad8D942eE13A198f6Ab49',
     },
     bsc: {
-        swapX2YModule: '0xe96526e92ee57bBD468DA1721987aa988b008768',
-        swapY2XModule: '0xbD6abA1Ef82A4cD6e15CB05e95f433ef48dfb5df',
-        liquidityModule: '0xF4efDB5A1E852f78e807fAE7100B1d38351e38c7',
-        limitOrderModule: '0x32D02Fc7722E81F6Ac60B87ea8B4b63a52Ad2b55',
-        flashModule: '0x110dE362cc436D7f54210f96b8C7652C2617887D',
-        factory: '0x1502d025BfA624469892289D45C0352997251728'
+        swapX2YModule: '0x2C6Df0fDbCE9D2Ded2B52A117126F2Dc991f770f',
+        swapY2XModule: '0x14323AfbC2b82fE58F0D9c203830EE969B4d1bE2',
+        liquidityModule: '0x88867BF3bB3321d8c7Da71a8eAb70680037068E4',
+        limitOrderModule: '0xB8dDAfE7385A962a4515821248368823E93fa61F',
+        flashModule: '0x77aB297Da4f3667059ef0C32F5bc657f1006cBB0',
+        factory: '0x4a41EbEa62E7aB70413356D30DF73cA803aaE41c'
     }
 }
